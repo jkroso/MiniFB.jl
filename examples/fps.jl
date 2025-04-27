742cbde917f23ca36fc0299981585834bb960805
Jake Rosoman <jkroso@gmail.com>
2025-04-27 23:33:28 +1000

    improve example

--- examples/fps.jl
+++ examples/fps.jl
@@ -1,29 +1,38 @@
-@use ".." frame onkey Window Keys KeyPress mm int px
+@use ".." @def frame onkey Window Keys KeyPress mm int px AbstractWindow Screen onresize onbuffer_resize
 @use Colors...
 
-const window = Window(title="Red and Blue", size=[200mm,300mm], animating=true)
+@def mutable struct FPSTest <: AbstractWindow
+  wave::UInt8=0
+  direction::Int8=3
+  time::Float64=time()
+  frames::UInt16=0
+end
 
-wave=0
-direction=4
-t = time()
-frames = 0
+const window = FPSTest(title="Red and Blue", size=Screen().size, animating=true)
 
-# animates between 2 colors and fills the buffer with just that color
-function frame(w::Window)
-  global wave, direction, t, frames
-  wave = clamp(wave+direction, 0, 256)
-  (0 < wave < 255) || (direction *= -1)
-  frames += 1
+function frame(w::FPSTest)
+  w.wave = w.wave + w.direction
+  (0 < w.wave < 255) || (w.direction = @fastmath -w.direction)
+  w.frames += 1%UInt16
   newtime = time()
-  if time() - t > 1
-    println("fsp: $frames")
-    frames=0
-    t = newtime
+  if newtime - w.time > 1
+    println("FPS: $(w.frames)")
+    w.frames = 0%UInt16
+    w.time = newtime
   end
-  fill!(w.buffer, RGBA{Colors.N0f8}(wave/256, 64/256, (256-wave)/256, 1))
+  fill!(w.buffer, RGBA{Colors.N0f8}(w.wave/256, 64/256, (256-w.wave)/256, 1))
 end
 
 # close when esc is pressed
-onkey(w::Window, ::KeyPress{Keys.escape}) = close(w)
+onkey(w::FPSTest, ::KeyPress{Keys.escape}) = close(w)
+
+onresize(w::FPSTest, size) = println("New size is: $(size[1]), $(size[2])")
+onbuffer_resize(w::FPSTest, size) = println("New buffer size is: $(size[1]), $(size[2])")
+
+println("""
+Press ESC to close the window. Resize the window to see how it affects the frame rate.
+Note: `isinteractive()` will have a big impact on FPS so don't run this example from
+a REPL
+""")
 
 open(window)
